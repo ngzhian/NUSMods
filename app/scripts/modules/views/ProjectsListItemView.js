8d9230fdb8ebcc8bdf6de593110197dd48449654
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-05-01 23:11:23 +0800

    Render project video thumbnails

--- app/scripts/modules/views/ProjectsListItemView.js
+++ app/scripts/modules/views/ProjectsListItemView.js
@@ -2,9 +2,18 @@
 
 var Marionette = require('backbone.marionette');
 var template = require('../templates/project_item.hbs');
+var videoParser = require('../../common/utils/videoParser.js');
 
 module.exports = Marionette.ItemView.extend({
   tagName: 'div',
   className: 'col-md-6',
-  template: template
+  template: template,
+  onShow: function () {
+    var videoUrl = this.model.get('videos')[0];
+    var that = this;
+    videoParser.getVideoThumbnail(videoUrl, function (thumbnail) {
+      that.model.set('thumbnail', thumbnail);
+      that.render();
+    });
+  }
 });
